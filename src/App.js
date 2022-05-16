28f105192d8ad633e27a6d21aee45148677d75b7
Gavin Loo <looweiren@gmail.com>
2022-05-16 19:03:55 +0800

    added sidebar left

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from "react"
 import Navbar from "./components/Navbar/Navbar"
 import Hero from "./components/Hero/Hero"
+import SidebarLeft from "./components/SidebarLeft/SidebarLeft"
 import colors from "./colors/colors.css"
 
 export default function App() {
     return (
         <div>
             <Navbar/>
-            <main>
-                <Hero/>
-            </main>
+            <div className="wrapper">
+                <SidebarLeft/>
+                <main>
+                    <Hero/>
+                </main>
+            </div>
+            
         </div>
     )
 }
---
+++ src/components/SidebarLeft/SidebarLeft.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { MdOutlineEmail } from "react-icons/md"
+import { BsLinkedin, BsGithub } from "react-icons/bs"
+
+export default function SidebarLeft() {
+    return (
+        <nav>
+            <ul>
+                <li><BsGithub/></li>
+                <li><BsLinkedin/></li>
+                <li><MdOutlineEmail/></li>
+            </ul>
+        </nav>
+    )
+}
